7fa6169cacdcd90adc8f3701588be439e40e1d7d
Richar <ripupo88@gmail.com>
2020-06-17 19:33:49 +0100

    Revert "working"
    This reverts commit 617d2e48fbdd5840863118387102342d773766b7.

--- src/App.js
+++ src/App.js
@@ -9,34 +9,15 @@ const style = {
   padding: 8,
 };
 
-let fetchingImgs = async(page)=>{
-
-  let imgArray = await fetch(`https://api.imgur.com/3/account/me/images/${page}`,
-  {headers:{Authorization:"Bearer 14e88fb8d72d08112c613f46f837d9b133e75264"}})
-  .then(res => res.json())
-  .catch(error => console.error('Error:', error))
-  .then(response => {    
-    return response.data
-  });
-  return imgArray;
-
-}
-
 function App() {
-  const [page, setPage] = useState(0);
-  const [item, setItem] = useState([{link:"https://i.imgur.com/gjPG9Hf.jpg"},{link:"https://i.imgur.com/gjPG9Hf.jpg"},{link:"https://i.imgur.com/gjPG9Hf.jpg"}]);
-  
-  console.log(item)
+  const [item, setItem] = useState(Array.from({ length: 20 }));
 
-  let fetchMoreData = async () => {
+  let fetchMoreData = () => {
     // a fake async api call like which sends
     // 20 more records in 1.5 secs
-    // setTimeout(() => {
-    //   setItem(item.concat(Array.from({ length: 20 })));
-    // }, 1500);
-    setPage(page+1);
-    setItem(item.concat(await fetchingImgs(page)));
-    
+    setTimeout(() => {
+      setItem(item.concat(Array.from({ length: 20 })));
+    }, 1500);
   };
 
   return (
@@ -52,7 +33,7 @@ function App() {
         loader={<h4>Loading...</h4>}
       >
         {item.map((i, index) => (
-          <img style={{maxWidth: "100%" }} src={i.link}></img>
+          <img src="./img/IMG-20171207-WA0000.jpg"></img>
         ))}
       </InfiniteScroll>
     </div>
